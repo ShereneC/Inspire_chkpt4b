4c6835ae31fe2f135fa40010b5f60c7d1f744112
ShereneC <spyhopper@cableone.net>
2021-10-19 13:36:18 -0600

    img

--- app/Controllers/ImagesController.js
+++ app/Controllers/ImagesController.js
@@ -3,24 +3,22 @@ import { imagesService } from "../Services/ImagesService.js"
 
 function _drawImage() {
   const image = ProxyState.activeImage
-  // document.body.style.backgroundImage = "url('${image}')";
-  // console.log('image from drawImage:' + image)
   let template = /*html*/`
-  
-  
-  `
-  
-//   <div
-//     class="col">
-//   <h1 id="time" class="text-center p-5 clock">TIME</h1>
-// </div>`
-  document.getElementById('body').innerHTML = template
-  // style="background-image: url('${image}'); background-size: cover"
-
-    //try this....
-    // document.getElementById('image').outerHTML = template
-    // then make the template change the <div to something like...
-    //<body class="debug bg-light" id="image" style="background-image: url('${image}'); background-size: cover">
+  <style>
+    body {
+      background-image: url('${image}');
+      background-size: cover;
+      background-position: center;
+    }
+    </style>  
+    `
+    document.getElementById('image').outerHTML = template
+  let clockTemplate = /*html*/`
+  <div
+    class="col">
+  <h1 id="time" class="text-center p-5 clock">TIME</h1>
+</div>`
+  document.getElementById('clock').innerHTML = clockTemplate
 }
 
 function _getTime() {
